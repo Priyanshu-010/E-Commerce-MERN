754ed8ec81fdb723f4fea1c8c33a9d972c6c0f63
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-06-12 15:24:02 +0530

    Cloudinary and createProduct Functionality added

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
+import { createProduct, getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../mdidleware/auth.middleware.js";
 
 const Router = express.Router();
 
 Router.get("/",protectRoute, adminRoute, getAllProducts);
 Router.get("/featured", getFeaturedProducts);
+Router.post("/",protectRoute, adminRoute, createProduct);
 
 export default Router
